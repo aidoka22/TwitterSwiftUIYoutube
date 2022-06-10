5a7838faddad25bb7cecfb36555860f74a7129ff
aidoka22 <aidana.sharipbay@gmail.com>
2022-06-10 19:17:52 +0600

    Initial Commit

---
+++ TwitterSwiftUIYoutube/TwitterSwiftUIYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIYoutubeApp.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by Айдана Шарипбай on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
